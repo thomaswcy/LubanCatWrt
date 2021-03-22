2bba8a7fa3f9c493d5723d8a0dce2ff6ae9b9d10
hejiawen <hjw0415@outlook.com>
2021-03-22 14:07:43 +0800

    更新一键升级脚本

--- make_update.sh
+++ make_update.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+echo '**************beginning update and install feeds***********'
+#更新升级软件包列表
 ./scripts/feeds update -a
 ./scripts/feeds install -a
+
+
+echo '**************application DoorNet1 defconfig***************'
+#使用DoorNet1默认配置
+cat defconfig >> .config
+make defconfig
+
+echo '**************download all packages***********************'
+#下载软件包
 make download V=s
-git checkout -- .config  
-#如果自己在menuconfig中修改了配置，使用#把第6行注释掉再执行此脚本
+
+###如果自己在menuconfig中修改了配置，
+###使用#把使用默认配置的命令注释掉再执行此脚本可一键更新软件包
+
