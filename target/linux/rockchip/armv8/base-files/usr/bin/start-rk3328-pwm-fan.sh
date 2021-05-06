f1c2cddb2ab0f42c26f547145d87d5bb6890b0a0
hejiawen <hjw0415@outlook.com>
2021-05-06 10:45:10 +0800

    fix:basefile 调整风扇pwm控制脚本，解决噪音问题

--- target/linux/rockchip/armv8/base-files/usr/bin/start-rk3328-pwm-fan.sh
+++ target/linux/rockchip/armv8/base-files/usr/bin/start-rk3328-pwm-fan.sh
@@ -17,48 +17,51 @@ ISENABLE=`cat /sys/class/pwm/pwmchip0/pwm0/enable`
 if [ $ISENABLE -eq 1 ]; then
     echo -n 0 > /sys/class/pwm/pwmchip0/pwm0/enable
 fi
-echo -n 50000 > /sys/class/pwm/pwmchip0/pwm0/period
+echo -n 2000000 > /sys/class/pwm/pwmchip0/pwm0/period
 echo -n 1 > /sys/class/pwm/pwmchip0/pwm0/enable
 
 # max speed run 5s
-echo -n 46990 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo -n 1900000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 sleep 5
-echo -n 25000 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
+echo -n 0 > /sys/class/pwm/pwmchip0/pwm0/duty_cycle
 
 # declare -a CpuTemps=(55000 43000 38000 32000)
 # declare -a PwmDutyCycles=(1000 20000 30000 45000)
 
-declare -a CpuTemps=(75000 63000 58000 52000)
-declare -a PwmDutyCycles=(25000 35000 45000 46990)
+declare -a CpuTemps=(65000 63000 60000 55000 45000)
+#declare -a PwmDutyCycles=(1000000 1500000 1700000 1900000 2000000)
+declare -a PwmDutyCycles=(2000000 1900000 1700000 1500000 1000000)
 
-declare -a Percents=(100 75 50 25)
-DefaultDuty=49990
+
+declare -a Percents=(100 95 85 75 50)
+DefaultDuty=0
 DefaultPercents=0
 
 while true
 do
-	temp=$(cat /sys/class/thermal/thermal_zone0/temp)
-	INDEX=0
-	FOUNDTEMP=0
-	DUTY=$DefaultDuty
-	PERCENT=$DefaultPercents
-	
-	for i in 0 1 2 3; do
-		if [ $temp -gt ${CpuTemps[$i]} ]; then
-			INDEX=$i
-			FOUNDTEMP=1
-			break
-		fi	
-	done
-	if [ ${FOUNDTEMP} == 1 ]; then
-		DUTY=${PwmDutyCycles[$i]}
-		PERCENT=${Percents[$i]}
-	fi
+        temp=$(cat /sys/class/thermal/thermal_zone0/temp)
+        INDEX=0
+        FOUNDTEMP=0
+        DUTY=$DefaultDuty
+        PERCENT=$DefaultPercents
 
-	echo -n $DUTY > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
+        for i in 0 1 2 3 4; do
+                if [ $temp -gt ${CpuTemps[$i]} ]; then
+                        INDEX=$i
+                        FOUNDTEMP=1
+                        break
+                fi
+        done
+        if [ ${FOUNDTEMP} == 1 ]; then
+                DUTY=${PwmDutyCycles[$i]}
+                PERCENT=${Percents[$i]}
+        fi
+
+        echo -n $DUTY > /sys/class/pwm/pwmchip0/pwm0/duty_cycle;
 
         echo "temp: $temp, duty: $DUTY, ${PERCENT}%"
         # cat /sys/devices/system/cpu/cpu*/cpufreq/cpuinfo_cur_freq
 
-	sleep 2s;
+        sleep 10s;
 done
+
